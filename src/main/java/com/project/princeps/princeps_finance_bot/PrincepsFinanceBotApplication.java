375e217ff77d0239723f948f2c6dcd7ee762fcee
imcode <128029980+ilyas11m@users.noreply.github.com>
2025-03-05 14:40:04 +0300

    Initial commit

---
+++ src/main/java/com/project/princeps/princeps_finance_bot/PrincepsFinanceBotApplication.java
@@ -0,0 +1,13 @@
+package com.project.princeps.princeps_finance_bot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrincepsFinanceBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PrincepsFinanceBotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/project/princeps/princeps_finance_bot/PrincepsFinanceBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.princeps.princeps_finance_bot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrincepsFinanceBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
