086b65d7fc824413d9a704dee6d4df832bd64b1f
imcode <128029980+ilyas11m@users.noreply.github.com>
2025-03-06 21:47:35 +0300

    logging main class

--- src/main/java/com/project/princeps/princeps_finance_bot/PrincepsFinanceBotApplication.java
+++ src/main/java/com/project/princeps/princeps_finance_bot/PrincepsFinanceBotApplication.java
@@ -1,13 +1,18 @@
 package com.project.princeps.princeps_finance_bot;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class PrincepsFinanceBotApplication {
-
+    public static final Logger logger = LoggerFactory.getLogger(PrincepsFinanceBotApplication.class);
     public static void main(String[] args) {
-        SpringApplication.run(PrincepsFinanceBotApplication.class, args);
+        try {
+            SpringApplication.run(PrincepsFinanceBotApplication.class, args);
+        } catch (Exception e) {
+            logger.error(e.getMessage());
+        }
     }
-
 }
