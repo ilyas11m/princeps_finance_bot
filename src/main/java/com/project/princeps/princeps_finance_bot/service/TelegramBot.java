c53ad5193a79a4db7ae850de01d0d8951627edf1
imcode <128029980+ilyas11m@users.noreply.github.com>
2025-03-07 23:22:54 +0300

    tg_user add method realised

--- src/main/java/com/project/princeps/princeps_finance_bot/service/TelegramBot.java
+++ src/main/java/com/project/princeps/princeps_finance_bot/service/TelegramBot.java
@@ -2,9 +2,10 @@ package com.project.princeps.princeps_finance_bot.service;
 
 import com.project.princeps.princeps_finance_bot.components.Buttons;
 import com.project.princeps.princeps_finance_bot.config.BotConfig;
+import com.project.princeps.princeps_finance_bot.model.TgUser;
 import com.project.princeps.princeps_finance_bot.utils.BotUtils;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.commands.SetMyCommands;
@@ -13,71 +14,41 @@ import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.api.objects.commands.BotCommand;
 import org.telegram.telegrambots.meta.api.objects.commands.scope.BotCommandScopeDefault;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
-import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
+@Slf4j
 @Service
 public class TelegramBot extends TelegramLongPollingBot {
 
-    BotConfig config;
+    private final TgUserService tgUserService;
 
-    Buttons buttons;
+    private final BotConfig config;
 
-    public static final Logger logger = LoggerFactory.getLogger(TelegramBot.class);
+    private final Buttons buttons;;
 
-    InlineKeyboardMarkup markup = new InlineKeyboardMarkup();
-
-    public TelegramBot(BotConfig config) {
+    @Autowired
+    public TelegramBot(BotConfig config, Buttons buttons,
+                       TgUserService tgUserService) {
         this.config = config;
-
-        List<List<InlineKeyboardButton>> rows = new ArrayList<>();
-        List<InlineKeyboardButton> firstRow = new ArrayList<>();
-        List<InlineKeyboardButton> secondRow = new ArrayList<>();
-        List<InlineKeyboardButton> thirdRow = new ArrayList<>();
-
-        InlineKeyboardButton helpButton = new InlineKeyboardButton(BotUtils.HELP_EMOJI + "\tПомощь");
-        helpButton.setCallbackData("HELP_BUTTON");
-        InlineKeyboardButton balanceButton = new InlineKeyboardButton(BotUtils.BALANCE + "\tБаланс");
-        balanceButton.setCallbackData("BALANCE_BUTTON");
-        InlineKeyboardButton incomeButton = new InlineKeyboardButton(BotUtils.INCOME + "\tДоход");
-        incomeButton.setCallbackData("INCOME_BUTTON");
-        InlineKeyboardButton expenseButton = new InlineKeyboardButton(BotUtils.EXPENSE + "\tРасход");
-        expenseButton.setCallbackData("EXPENSE_BUTTON");
-        InlineKeyboardButton budgetButton = new InlineKeyboardButton(BotUtils.BUDGET + "\tБюджет");
-        budgetButton.setCallbackData("BUDGET_BUTTON");
-        InlineKeyboardButton reportButton = new InlineKeyboardButton(BotUtils.REPORT + "\tОтчёт");
-        reportButton.setCallbackData("REPORT_BUTTON");
-        InlineKeyboardButton notificationButton = new InlineKeyboardButton(BotUtils.BELL_EMOJI + "\tУведомления");
-        notificationButton.setCallbackData("NOTIFICATION_BUTTON");
-        InlineKeyboardButton settingsButton = new InlineKeyboardButton(BotUtils.SETTINGS + "\tНастройки");
-        settingsButton.setCallbackData("SETTINGS_BUTTON");
-
-        firstRow.add(helpButton);
-        firstRow.add(budgetButton);
-
-        secondRow.add(incomeButton);
-        secondRow.add(expenseButton);
-        secondRow.add(budgetButton);
-
-        thirdRow.add(reportButton);
-        thirdRow.add(notificationButton);
-        thirdRow.add(settingsButton);
-
-        rows.add(firstRow);
-        rows.add(secondRow);
-        rows.add(thirdRow);
-
-        markup.setKeyboard(rows);
+        this.buttons = buttons;
+        this.tgUserService = tgUserService;
 
         List<BotCommand> botCommandList = new ArrayList<>();
         botCommandList.add(new BotCommand("/start", "get a start message"));
+        botCommandList.add(new BotCommand("/menu", "get a bot's available functionality"));
+        botCommandList.add(new BotCommand("/register", "register your profile"));
         try {
             execute(new SetMyCommands(botCommandList, new BotCommandScopeDefault(), null));
         }
-        catch (Exception e) {}
+        catch (Exception e) {
+            log.error(e.getMessage());
+        }
+
     }
 
     @Override
@@ -90,28 +61,69 @@ public class TelegramBot extends TelegramLongPollingBot {
         return config.token;
     }
 
+
     @Override
     public void onUpdateReceived(Update update) {
         if (update.hasMessage() && update.getMessage().hasText()) {
             long chatId = update.getMessage().getChatId();
             String command = update.getMessage().getText();
             switch (command) {
+
+                case "/register":
+                    LocalDateTime current = LocalDateTime.now();
+                    Timestamp time = Timestamp.valueOf(current);
+                    Long tgUserId = update.getMessage().getChat().getId();
+                    String username = update.getMessage().getChat().getUserName();
+                    tgUserRegistration(update,time,tgUserId,username);
+                    break;
                 case "/start":
                     sendMessage(chatId, BotUtils.GREETING, "HTML");
                     break;
-                case "Помощь":
-                    sendMessage(chatId, BotUtils.HELP_TEXT, "HTML");
+                case "/menu":
+                    sendMessage(chatId, BotUtils.MENU_TEXT, "HTML", buttons.setMarkup());
                     break;
             }
         }
+
         else if (update.hasCallbackQuery()) {
             String callData = update.getCallbackQuery().getData();
             long chatId = update.getCallbackQuery().getMessage().getChatId();
+
             switch (callData) {
                 case "HELP_BUTTON":
-                    sendMessage(chatId, BotUtils.HELP_TEXT, "HTML");
+                    sendMessage(chatId, BotUtils.HELP_TEXT, "HTML", buttons.setMarkup());
                     break;
             }
+        }
+    }
+
+    public void tgUserRegistration(Update update, Timestamp time, Long tgUserId, String username) {
+        long chatId = update.getMessage().getChatId();
+        TgUser tgUser = new TgUser();
+        tgUser.setTg_user_id(tgUserId);
+        tgUser.setUsername(username);
+        tgUser.setTimeRegistration(time);
+        try {
+            tgUserService.addTgUser(tgUser);
+            log.info("SUCCESS, TG USER ADDED");
+            sendMessage(chatId, "Вы успешно <u>зарегистрировались</u>!\t" + BotUtils.MARK, "HTML");
+        } catch (Exception e) {
+            sendMessage(chatId, "Вы уже <u>зарегистрированы</u>!\t" + BotUtils.X, "HTML");
+            log.error(e.getMessage());
+        }
+    }
+
+    public void sendMessage(long chatId, String text, String parseMode, InlineKeyboardMarkup markup) {
+        SendMessage message = new SendMessage();
+        message.setChatId(String.valueOf(chatId));
+        message.setText(text);
+        message.enableHtml(true);
+        message.setParseMode(parseMode);
+        message.setReplyMarkup(markup);
+        try {
+            execute(message);
+        } catch (TelegramApiException e) {
+            log.error(e.getMessage());
         }
     }
 
@@ -121,12 +133,11 @@ public class TelegramBot extends TelegramLongPollingBot {
         message.setText(text);
         message.enableHtml(true);
         message.setParseMode(parseMode);
-        message.setReplyMarkup(markup);
 
         try {
             execute(message);
         } catch (TelegramApiException e) {
-            logger.error(e.getMessage());
+            log.info(e.getMessage());
         }
     }
 }
