a4b7613ef625ec57d2604643fd2d14d9a5e3c77c
imcode <128029980+ilyas11m@users.noreply.github.com>
2025-03-07 23:24:16 +0300

    lombok annotation for log messages

--- src/main/java/com/project/princeps/princeps_finance_bot/PrincepsFinanceBotApplication.java
+++ src/main/java/com/project/princeps/princeps_finance_bot/PrincepsFinanceBotApplication.java
@@ -1,18 +1,17 @@
 package com.project.princeps.princeps_finance_bot;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@Slf4j
 @SpringBootApplication
 public class PrincepsFinanceBotApplication {
-    public static final Logger logger = LoggerFactory.getLogger(PrincepsFinanceBotApplication.class);
     public static void main(String[] args) {
         try {
             SpringApplication.run(PrincepsFinanceBotApplication.class, args);
         } catch (Exception e) {
-            logger.error(e.getMessage());
+            log.error(e.getMessage());
         }
     }
 }
