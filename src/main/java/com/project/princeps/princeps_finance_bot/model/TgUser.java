54f33325ea4dd88ce6414a2591bf82ccd39f4ed7
imcode <128029980+ilyas11m@users.noreply.github.com>
2025-03-13 16:01:04 +0300

    tg_user_id set unique value

--- src/main/java/com/project/princeps/princeps_finance_bot/model/TgUser.java
+++ src/main/java/com/project/princeps/princeps_finance_bot/model/TgUser.java
@@ -4,6 +4,7 @@ import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
+import org.checkerframework.common.aliasing.qual.Unique;
 import org.hibernate.annotations.ColumnDefault;
 
 import java.sql.Timestamp;
@@ -24,6 +25,7 @@ public class TgUser {
 
     @Size(max = 50)
     @NotNull
+    @Unique
     @Column(name = "tg_user_id", nullable = false, length = 50)
     private Long tg_user_id;
 
