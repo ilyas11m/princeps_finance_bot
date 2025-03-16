648d29ae5699f61bcbac0172348921d9a391d6cd
imcode <128029980+ilyas11m@users.noreply.github.com>
2025-03-16 17:01:40 +0300

    sevice for budget

---
+++ src/main/java/com/project/princeps/princeps_finance_bot/service/BudgetService.java
@@ -0,0 +1,26 @@
+package com.project.princeps.princeps_finance_bot.service;
+
+import com.project.princeps.princeps_finance_bot.model.Budget;
+import com.project.princeps.princeps_finance_bot.repository.BudgetRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.jvnet.hk2.annotations.Service;
+
+@Slf4j
+@Service
+public class BudgetService {
+
+    BudgetRepository budgetRepository;
+
+    public BudgetService(BudgetRepository budgetRepository) {
+        this.budgetRepository = budgetRepository;
+    }
+
+    public void add(Budget budget) {
+        try {
+            budgetRepository.save(budget);
+        }
+        catch (Exception e) {
+            log.error("");
+        }
+    }
+}
