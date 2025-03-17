4d4d97a197d1678287600343f36fc410f1b84e7d
imcode <128029980+ilyas11m@users.noreply.github.com>
2025-03-17 15:41:42 +0300

    добавлены методы добавления баланса, лимита

--- src/main/java/com/project/princeps/princeps_finance_bot/service/TelegramBot.java
+++ src/main/java/com/project/princeps/princeps_finance_bot/service/TelegramBot.java
@@ -2,6 +2,7 @@ package com.project.princeps.princeps_finance_bot.service;
 
 import com.project.princeps.princeps_finance_bot.components.Buttons;
 import com.project.princeps.princeps_finance_bot.config.BotConfig;
+import com.project.princeps.princeps_finance_bot.model.Budget;
 import com.project.princeps.princeps_finance_bot.model.TgUser;
 import com.project.princeps.princeps_finance_bot.utils.BotUtils;
 import lombok.extern.slf4j.Slf4j;
@@ -17,38 +18,45 @@ import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMa
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 import java.sql.Timestamp;
+import java.time.Instant;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 @Slf4j
 @Service
 public class TelegramBot extends TelegramLongPollingBot {
 
+    private final BudgetService budgetService;
+
     private final TgUserService tgUserService;
 
     private final BotConfig config;
 
-    private final Buttons buttons;;
+    private final Buttons buttons;
+
+    private HashMap<Long, String> dataState = new HashMap<>();
 
     @Autowired
     public TelegramBot(BotConfig config, Buttons buttons,
-                       TgUserService tgUserService) {
+                       TgUserService tgUserService, BudgetService budgetService) {
         this.config = config;
         this.buttons = buttons;
         this.tgUserService = tgUserService;
+        this.budgetService = budgetService;
 
         List<BotCommand> botCommandList = new ArrayList<>();
         botCommandList.add(new BotCommand("/start", "get a start message"));
-        botCommandList.add(new BotCommand("/menu", "get a bot's available functionality"));
         botCommandList.add(new BotCommand("/register", "register your profile"));
+        botCommandList.add(new BotCommand("/budget", "set your budget"));
+
         try {
             execute(new SetMyCommands(botCommandList, new BotCommandScopeDefault(), null));
         }
         catch (Exception e) {
             log.error(e.getMessage());
         }
-
     }
 
     @Override
@@ -61,26 +69,42 @@ public class TelegramBot extends TelegramLongPollingBot {
         return config.token;
     }
 
-
     @Override
     public void onUpdateReceived(Update update) {
         if (update.hasMessage() && update.getMessage().hasText()) {
             long chatId = update.getMessage().getChatId();
             String command = update.getMessage().getText();
+            String balanceButton = "Баланс";
+            Long tgUserId = update.getMessage().getChat().getId();
+            if (command.contains(balanceButton)) {
+                dataState.put(chatId, "balance_response");
+                sendMessage(chatId, BotUtils.BUDGET_UPDATE, "HTML");
+                sendMessage(chatId, "Введите баланс:", null);
+                return;
+            }
             switch (command) {
-
+                case "/start":
+                    sendMessage(chatId, BotUtils.GREETING, "HTML", buttons.inlineMarkup());
+                    break;
                 case "/register":
                     LocalDateTime current = LocalDateTime.now();
                     Timestamp time = Timestamp.valueOf(current);
-                    Long tgUserId = update.getMessage().getChat().getId();
                     String username = update.getMessage().getChat().getUserName();
                     tgUserRegistration(update,time,tgUserId,username);
                     break;
-                case "/start":
-                    sendMessage(chatId, BotUtils.GREETING, "HTML");
-                    break;
-                case "/menu":
-                    sendMessage(chatId, BotUtils.MENU_TEXT, "HTML", buttons.setMarkup());
+                default:
+                    if (dataState.containsKey(chatId) && dataState.get(chatId).equals("balance_response")) {
+                        try {
+                            long balance = Long.valueOf(update.getMessage().getText());
+                            long limit = 123;
+                            Timestamp timestamp = Timestamp.from(Instant.now());
+                            budgetCreation(update, tgUserId, balance, limit, timestamp);
+                            dataState.remove(chatId);
+                        } catch (Exception e) {
+                            sendMessage(chatId, "Некорректный ввод!", "HTML");
+                            log.error(e.getMessage());
+                        }
+                    }
                     break;
             }
         }
@@ -89,18 +113,32 @@ public class TelegramBot extends TelegramLongPollingBot {
             String callData = update.getCallbackQuery().getData();
             long chatId = update.getCallbackQuery().getMessage().getChatId();
 
-            switch (callData) {
-                case "HELP_BUTTON":
-                    sendMessage(chatId, BotUtils.HELP_TEXT, "HTML", buttons.setMarkup());
-                    break;
+            if (callData.equals("HELP_BUTTON")) {
+                sendMessage(chatId, BotUtils.HELP_TEXT, "HTML");
             }
+        }
+    }
+
+    public void budgetCreation(Update update, long tgUserId, long balance, long limit,
+                               Timestamp periodBegin) {
+        long chatId = update.getMessage().getChatId();
+        Budget budget = new Budget();
+        budget.setUserId(tgUserService.findById(tgUserId));
+        budget.setBalance(balance);
+        budget.setLimitAmount(limit);
+        budget.setPeriodBegin(periodBegin);
+        try {
+            budgetService.add(budget);
+            sendMessage(chatId, "Бюджет обновлен!", "HTML");
+        } catch (Exception e) {
+            log.error(e.getMessage());
         }
     }
 
     public void tgUserRegistration(Update update, Timestamp time, Long tgUserId, String username) {
         long chatId = update.getMessage().getChatId();
         TgUser tgUser = new TgUser();
-        tgUser.setTg_user_id(tgUserId);
+        tgUser.setTgUserId(tgUserId);
         tgUser.setUsername(username);
         tgUser.setTimeRegistration(time);
         try {
@@ -127,12 +165,14 @@ public class TelegramBot extends TelegramLongPollingBot {
         }
     }
 
+
     public void sendMessage(long chatId, String text, String parseMode) {
         SendMessage message = new SendMessage();
         message.setChatId(String.valueOf(chatId));
         message.setText(text);
         message.enableHtml(true);
         message.setParseMode(parseMode);
+        message.setReplyMarkup(buttons.replyMarkup());
 
         try {
             execute(message);
