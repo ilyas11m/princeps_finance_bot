b73a11957f54d9152735f75902dac9689e44c851
imcode <128029980+ilyas11m@users.noreply.github.com>
2025-03-17 15:42:09 +0300

    добавлен методы поиска по колонке

--- src/main/java/com/project/princeps/princeps_finance_bot/repository/TgUserRepository.java
+++ src/main/java/com/project/princeps/princeps_finance_bot/repository/TgUserRepository.java
@@ -3,5 +3,11 @@ package com.project.princeps.princeps_finance_bot.repository;
 import com.project.princeps.princeps_finance_bot.model.TgUser;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
 public interface TgUserRepository extends JpaRepository<TgUser, Long> {
+    Optional<TgUser> findByTgUserId(long tgUserId);
 }
