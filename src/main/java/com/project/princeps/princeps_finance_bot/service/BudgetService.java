2eb3e9a79e23052c6cb9371af536b9757fffb198
imcode <128029980+ilyas11m@users.noreply.github.com>
2025-03-17 15:43:29 +0300

    логи ошибок

--- src/main/java/com/project/princeps/princeps_finance_bot/service/BudgetService.java
+++ src/main/java/com/project/princeps/princeps_finance_bot/service/BudgetService.java
@@ -3,7 +3,7 @@ package com.project.princeps.princeps_finance_bot.service;
 import com.project.princeps.princeps_finance_bot.model.Budget;
 import com.project.princeps.princeps_finance_bot.repository.BudgetRepository;
 import lombok.extern.slf4j.Slf4j;
-import org.jvnet.hk2.annotations.Service;
+import org.springframework.stereotype.Service;
 
 @Slf4j
 @Service
@@ -18,9 +18,10 @@ public class BudgetService {
     public void add(Budget budget) {
         try {
             budgetRepository.save(budget);
+            log.info("SUCCESS, BUDGET UPDATED");
         }
         catch (Exception e) {
-            log.error("");
+            log.error(e.getMessage());
         }
     }
 }
